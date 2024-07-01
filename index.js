ecfbdf848c9210ff92380ddb154480c1ac338e89
kunal latta <kunalofficial234@gmail.com>
2024-07-01 16:14:56 +0530

    vowel count

--- index.js
+++ index.js
@@ -1,36 +1,12 @@
-let t_alpha_c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let t_alpha_s = "abcdefghijklmnopqrstuvwxyz";
-let t_sym = "!@#$%^&*()_+-=[]{}|;:'\",.<>?/`~";
-let t_num = "1234567890";
+document.getElementById("btn").addEventListener('click', ()=>{
 
-let length = document.getElementById("length");
-let low = document.getElementById("lowercase");
-let upp = document.getElementById("uppercase");
-let num = document.getElementById("number");
-let sam = document.getElementById("symbols");
-
-document.getElementById("btn").addEventListener('click',() =>{
-
-  let char = "";
-  let zoom = "";
-  let total_length = parseInt(length.value);
-
-  if(low.checked){
-    char +=t_alpha_s;
+  let s = document.getElementById("text").value;
+  let arr = ['a','e','i','o','u'];
+  let count = 0;
+  for(let i=0;i<s.length;i++){
+    if(arr.includes(s[i])){
+      count++;
+    }
   }
-  if(upp.checked){
-    char += t_alpha_c;
-  }
-  if(num.checked){
-    char +=t_num;
-  }
-  if(sam.checked){
-    char += t_sym;
-  }
-
-  for(let i=0;i<total_length;i++){
-    zoom += char.charAt(Math.floor(Math.random()*char.length));
-  }
-
-  document.getElementById("result").value = zoom;
+  document.getElementById("result").innerText = `Total Vowels: ${count}`;
 })
